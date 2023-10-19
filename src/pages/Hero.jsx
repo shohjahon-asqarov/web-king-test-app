ce8bfb3eb4900e5e8f5fb177d1c085e479f4653b
Shohjahon <104879567+shohjahon-asqarov@users.noreply.github.com>
2023-10-19 08:08:30 +0500

    add hero

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -5,13 +5,13 @@ import { Link } from 'react-router-dom'
 const Hero = () => {
   return (
     <section className='container'>
-      <div className="grid grid-cols-7 gap-10 items-center">
-        <div className='space-y-5 col-span-4'>
-          <h1 className='text-6xl font-bold text-gray-900'>
-            <span className='text-yellow'>Web king</span> <br />
+      <div className="grid md:grid-cols-7 gap-x-10 gap-y-5 items-center py-10 md:py-0">
+        <div className='space-y-5 md:col-span-4 text-center'>
+          <h1 className='text-4xl md:text-6xl font-bold text-gray-900'>
+            <span className='text-purple'>Web king</span> <br />
             o'quv markazi
           </h1>
-          <p className='text-gray-700 text-lg'>
+          <p className='text-gray-700 md:text-lg'>
             Ta'lim va atrof muhitga inavatsion yordam berish orqali
           </p>
           <Link to='/category' className='btn-blue inline-block space-x-2 py-3.5 px-8'>
@@ -20,7 +20,7 @@ const Hero = () => {
           </Link>
         </div>
 
-        <div className='col-span-3'>
+        <div className='md:col-span-3'>
           <img src={heroImg} alt="hero" />
         </div>
       </div>
