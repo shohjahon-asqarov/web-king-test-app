21c8fb484873bd72814cc0a04a46d70577b46ca4
Shohjahon <104879567+shohjahon-asqarov@users.noreply.github.com>
2023-10-25 07:52:55 +0500

    add sort category

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -9,6 +9,7 @@ import { useDispatch } from 'react-redux';
 import { setQuestionsState } from '../store/slices/questionsSlice';
 import { useNavigate } from 'react-router-dom';
 import Loader from '../components/Loader';
+import { Empty, Select } from 'antd';
 
 const Categories = () => {
 
@@ -18,6 +19,8 @@ const Categories = () => {
     const [loader, setLoader] = useState(true)
 
     const [questions, setQuestions] = useState([])
+
+    const [selectedCategory, setSelectedCategory] = useState('All')
 
     const fetchData = async () => {
         const q = query(collection(db, 'questions'));
@@ -40,17 +43,53 @@ const Categories = () => {
         navigate('/test')
     }
 
+    const changeCategory = (value) => {
+        setSelectedCategory(value)
+    };
+
+    const found = questions.find(question => question.owner.category === selectedCategory)
+
     return (
         <div className='container pb-16'>
             {
                 loader
                     ? <Loader />
                     : <div>
-                        <h1 className='text-center py-10 text-3xl font-semibold'>Testlar</h1>
+                        <h2 className='text-center py-10 text-3xl font-semibold'>Testlar</h2>
+
+                        <div className='mb-10 flex justify-between items-center'>
+                            <h2 className='text-xl md:text-3xl font-semibold'>Filter</h2>
+                            <Select
+                                defaultValue="All"
+                                style={{
+                                    width: 120,
+                                }}
+                                onChange={changeCategory}
+                                options={[
+                                    {
+                                        value: 'All',
+                                        label: 'Barchasi',
+                                    },
+                                    {
+                                        value: 'English',
+                                        label: 'English',
+                                    },
+                                    {
+                                        value: 'Front-end',
+                                        label: 'Front-end',
+                                    },
+                                    {
+                                        value: 'Back-end',
+                                        label: 'Back-end',
+                                    },
+                                ]}
+                            />
+                        </div>
+
                         <ul className='grid sm:grid-cols-2 lg:grid-cols-3 gap-8'>
                             {questions && questions.map((i, index) => {
                                 return (
-                                    <li data-aos='fade-up' className='p-7 rounded-3xl cursor-pointer bg-card-bg border border-[hsla(0,0%,87.5%,.7)] text-[rgba(2,11,18,.7)] hover:bg-slate-200 !transition-colors duration-300' key={index}>
+                                    <li className={`p-7 rounded-3xl cursor-pointer bg-card-bg border border-[hsla(0,0%,87.5%,.7)] text-[rgba(2,11,18,.7)] hover:bg-slate-200 !transition-colors duration-300 ${i.owner.category === selectedCategory || selectedCategory === 'All' ? 'inline-block' : 'hidden'}`} key={index}>
                                         <p className='mb-3'><i className='bi bi-person mr-2'></i> {i.owner.name ? i.owner.name : 'No Author'}</p>
                                         <h3 className='uppercase mb-3 text-xl font-bold !text-[#289C8E]'>
                                             <i className="bi bi-award mr-2 text-base"></i>
@@ -73,6 +112,7 @@ const Categories = () => {
                                 )
                             })}
                         </ul>
+                        {!found && selectedCategory !== 'All' && <Empty description='Test mavjud emas' />}
                     </div>
             }
         </div>
