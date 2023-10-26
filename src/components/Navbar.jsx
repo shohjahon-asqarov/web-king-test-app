922619381fc40c1312367269a04fdea618ff0d65
Shohjahon <104879567+shohjahon-asqarov@users.noreply.github.com>
2023-10-26 15:52:07 +0500

    change blue button styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { NavLink } from 'react-router-dom'
 const Navbar = () => {
     return (
         <header className='sticky top-0 bg-white z-50 border-b shadow-sm'>
-            <div className="container flex justify-between py-6 items-center">
+            <div className="container flex justify-between py-5 md:py-6 items-center">
                 <NavLink to="/">
                     <img className='w-32 sm:w-44' src={logo} alt="web king logo" />
                 </NavLink>
