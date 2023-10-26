2e24cebd66e5db7fcd9e8efcc4e40af9cb2adbb3
Shohjahon <104879567+shohjahon-asqarov@users.noreply.github.com>
2023-10-26 15:56:24 +0500

    change hero description

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -27,7 +27,7 @@ const Hero = () => {
             </span>
           </h1>
           <p className='text-gray-700 md:text-lg'>
-            Ta'lim va atrof muhitga inavatsion yordam berish orqali har bir inson hayotini yaxshilash
+            Pastdagi tugmani bosing va blimingizni sinab ko'ring
           </p>
           <Link to='/category' className='btn-blue inline-block space-x-2 py-3.5 px-8'>
             <span>Boshlamoq</span>
